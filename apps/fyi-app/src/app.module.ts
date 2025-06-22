d81d0aa2177daf210c092263c0472092e242a0dc
Persephone <persephone.dechario@gmail.com>
2025-06-21 23:52:05 -0400

    Add deployment.

--- apps/fyi-app/src/app.module.ts
+++ apps/fyi-app/src/app.module.ts
@@ -1,22 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from '@fyi-timescale/typeorm.config';  
 import { TimescaleModule, Metric } from '@fyi-timescale';
 import { MetricsController } from '../metrics.controller';  
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432'),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [Metric],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
     TimescaleModule,
   ],
   controllers: [MetricsController],
